faca7dba4b1ef42195320cbeabe753714e9d3fa3
eldin001 <eldin.balje@icloud.com>
2024-03-15 13:41:10 +0100

    test square

---
+++ script.js
@@ -0,0 +1,17 @@
+/* L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+Ogni cella ha un numero progressivo, da 1 a 100.
+Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Ad esempio:
+Di cosa ho bisogno per generare i numeri?
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli anche in un secondo momento. */
+
+//! seleziono il bottone e lo salvo in una variabile
+let buttonElement = document.getElementById('button');
+console.log(buttonElement);
+//!comportamento una volta premuto il bottone
+buttonElement.addEventListener('click', function(){
+    
+})
